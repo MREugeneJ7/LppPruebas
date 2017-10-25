f908db96204c6fae3247c405115d1d6a0c7f1653
mreugenej7 <eumadeinchina@gmail.com>
2017-10-25 08:58:51 +0000

    Creada la clase alimento y comprobado que tiene atributos

--- lib/Alimento.rb
+++ lib/Alimento.rb
@@ -1,5 +1,22 @@
 require "Alimento/version"
 
-module Alimento
-  # Your code goes here...
+class Alimento
+    attr_reader :nombre, :glucidos, :proteinas, :lipidos
+    
+    def initialize(nombre, glucidos, proteinas, lipidos)
+        @nombre = nombre
+        @glucidos = glucidos
+        @proteinas = proteinas
+        @lipidos = lipidos
+    end
+    
+    def to_s
+        x = "#{nombre}: #{proteinas}g proteínas, #{glucidos}g glúcidos, #{lipidos}g lípidos"
+        return x
+    end
+    
+    def kcal
+        @proteinas * 4 + @glucidos * 4 + @lipidos * 9
+    end
+
 end
--- spec/Alimento_spec.rb
+++ spec/Alimento_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe Alimento do
   it "has a version number" do
     expect(Alimento::VERSION).not_to be nil
   end
+  context "Atributos" do
+    it "Tiene los atributos nombre, proteinas, glucidos y lipidos" do
+      expect(Alimento.new("manzana", 10, 10, 10)).to have_attributes(:nombre => "manzana", :proteinas => 10, :glucidos => 10, :lipidos => 10)
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
   end
+  
 end
