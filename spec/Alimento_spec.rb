3b56ac83b8ebc381897f2564f1fb260c06127637
mreugenej7 <eumadeinchina@gmail.com>
2017-10-25 09:01:47 +0000

    Comprobado que se pueden obtener los atributos

--- spec/Alimento_spec.rb
+++ spec/Alimento_spec.rb
@@ -8,7 +8,17 @@ RSpec.describe Alimento do
     it "Tiene los atributos nombre, proteinas, glucidos y lipidos" do
       expect(Alimento.new("manzana", 10, 10, 10)).to have_attributes(:nombre => "manzana", :proteinas => 10, :glucidos => 10, :lipidos => 10)
     end
-
-  end
-  
+  it "Tiene un método para obtener el nombre" do
+      expect(Alimento.new("manzana", 10, 10, 10).nombre).to eq("manzana")
+    end
+    it "Tiene un método para obtener el glucidos" do
+      expect(Alimento.new("manzana", 10, 10, 10).glucidos).to eq(10)
+    end
+    it "Tiene un método para obtener el proteinas" do
+      expect(Alimento.new("manzana", 10, 10, 10).proteinas).to eq(10)
+    end
+    it "Tiene un método para obtener el lipidos" do
+      expect(Alimento.new("manzana", 10, 10, 10).lipidos).to eq(10)
+    end
 end
+end 
