df0900e2c813a5d89afffca545999159f8a1d02a
mreugenej7 <eumadeinchina@gmail.com>
2017-11-22 00:43:39 +0000

    tiene atributo indice glucemico

--- alimento/lib/alimento/Lista.rb
+++ alimento/lib/alimento/Lista.rb
@@ -1,6 +1,6 @@
 # Representación de un nodo en una lista
 #
-# @author Sebastian Jose Diaz Rodriguez
+# @author Eugenio Jose Gonzalez Luis
 # @since 1.0.0
 # @attr_accessor any value Informacion contenida en el nodo
 # @attr_accessor Node next Nodo siguiente con el que esta conectado
@@ -9,7 +9,7 @@ Node = Struct.new(:value, :next, :prev)
 
 # Representación de una lista doblemente enlazada de nodos
 # 
-# @author Sebastian Jose Diaz Rodriguez
+# @author Eugenio Jose Gonzalez Luis
 # @since 1.0.0
 # @attr_reader Node head Nodo en la cabeza de la lista
 # @attr_reader Node tail Nodo en la última posición de la lista
